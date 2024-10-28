8858b6e64d76defcb3cf297a1af0a123ad0e2037
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-10-28 14:21:21 -0300

    Update default.nix version

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 
 pkgs.stdenv.mkDerivation  rec {
     pname = "llvm-mos-sdk";
-    version = "9.0.1";
+    version = "20.2.0";
     src = pkgs.fetchurl {
         url = "https://github.com/llvm-mos/llvm-mos-sdk/releases/download/v20.2.0/llvm-mos-linux.tar.xz";
         hash = "sha256-sObbnNChtVeQ2xE9HFaL/+DWL+TwD3CxoZ4Wsy/G3Pc=";
